16f437df29ca47ce1f850c22193638991745cdd2
suganthiarumugam007 <99194517+suganthiarumugam007@users.noreply.github.com>
2022-04-22 16:49:37 +0530

    Create List Operations.py

---
+++ Collections/List Operations.py
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5,6,7,8]
+b=[11,22,33]
+print("1. Find the list item in position 2 is:", a[2])
+print("2. Print the total number of item in a list:", len(a))
+print("3. Display the 4th to 6th elements in the list:",a[3:6])
+print("4. Display the first 3 items:", a[:3])
+print("5. Display the last 3 elements:", a[-3:])
+print("6. Display the last item in list:", a[-1])
+print("7. Print element in the list:", a)
+print("8. Concatenate two list:", a+b)
+print("9. Print the middle number:", a[3:-3])
